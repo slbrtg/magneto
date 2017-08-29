2318c54382b59a944b8feea02ea428cef1e011e4
saul <slbrtg@gmail.com>
2017-08-29 15:48:02 -0700

    make progress on magnet link security issue

--- src/app/torrent-list/torrent-list.component.ts
+++ src/app/torrent-list/torrent-list.component.ts
@@ -3,6 +3,8 @@ import { Torrent } from '../torrent.model';
 import { Router } from '@angular/router';
 import { TorrentService } from '../torrent.service';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-torrent-list',
@@ -13,15 +15,26 @@ import { FirebaseListObservable } from 'angularfire2/database';
 export class TorrentListComponent implements OnInit {
   allTorrents: FirebaseListObservable<any[]>;
   currentRoute: string = this.router.url;
+  safeUrl;
 
-  constructor(private router: Router, private torrentService: TorrentService) { }
+  constructor(private router: Router, private torrentService: TorrentService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
-    this.allTorrents = this.torrentService.getAllTorrents();
+      this.allTorrents = this.torrentService.getAllTorrents();
+      // console.log(this.allTorrents);
+      // setTimeout(function(){
+      //   this.sanitizeUrl(this.allTorrents)
+      // }.bind(this), 1000);
   }
 
   torrentDetailPage(clickedTorrent) {
     this.router.navigate(['torrent-page', clickedTorrent.$key]);
   }
+
+  // sanitizeUrl(torrents){
+  //   for (let key in torrents){
+  //     this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(torrents[key].magnet);
+  //   }
+  // }
 
 }
