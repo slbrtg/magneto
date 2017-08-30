379775762d228c7bc5193a9406889af948345ea1
Joel Bakken <joel@>
2017-08-30 10:42:19 -0700

    edit

--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -1,8 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Torrent } from './torrent.model';
 
-
-
 @Pipe({
   name: 'search',
   pure: false
@@ -12,7 +10,7 @@ export class SearchPipe implements PipeTransform {
   transform(torrents: any[], input: any): any {
     if (input === undefined) return torrents;
     return torrents.filter(function(torrent) {
-      return torrent.name.toLowerCase().includes(input.toLowerCase()) || torrent.category.toLowerCase().includes(input.toLowerCase()); 
+      return torrent.name.toLowerCase().includes(input.toLowerCase()) || torrent.category.toLowerCase().includes(input.toLowerCase());
     })
   }
 }
